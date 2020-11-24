48ca858fd2b0b521add5ece635656c5cd12f75b5
박영호 <mellonia.bluerose@gmail.com>
2020-11-24 21:21:40 +0900

    직선 웨이브

--- wave/Wave.js
+++ wave/Wave.js
@@ -1,8 +1,11 @@
 import { Point } from './Point.js';
 
 export class Wave {
-    constructor() {
-
+    constructor(index, totalPoints, color) {
+        this.index = index;
+        this.totalPoints = totalPoints;
+        this.color = color;
+        this.points = [];
     }
 
     // 애니메이션을 그릴 때 가장 중요한 건 그리고자 하는 것의 좌표값을 가지고 와야한다.
@@ -13,24 +16,50 @@ export class Wave {
         this.centerX = stageWidth / 2;
         this.centerY = stageHeight / 2;
 
+        this.pointGap = this.stageWidth / (this.totalPoints - 1);
+
         this.init();
     }
 
     init() {
-        this.point = new Point(
-            this.centerX,
-            this.centerY
-        );
+        this.points = [];
+
+        for(let i = 0; i < this.totalPoints; i++) {
+            const point = new Point(
+                this.index + i,
+                this.pointGap * i,
+                this.centerY
+            );
+            this.points[i] = point;
+        }
     }
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.fillStyle = "#ff0000";
-
-        this.point.update();
+        ctx.fillStyle = this.color;
         
-        ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
-        ctx.fill();
+        let prevX = this.points[0].x;
+        let prevY = this.points[0].y;
 
+        ctx.moveTo(prevX, prevY);
+
+        for (let i = 1; i < this.totalPoints; i++) {
+            if (i < this.totalPoints - 1) {
+                this.points[i].update();
+            }
+
+            const cx = (prevX + this.points[i].x) / 2;
+            const cy = (prevY + this.points[i].y) / 2;
+
+            ctx.lineTo(cx, cy);
+
+            prevX = this.points[i].x;
+            prevY = this.points[i].y;
+        }
+        ctx.lineTo(prevX, prevY);
+        ctx.lineTo(this.stageWidth, this.stageHeight);
+        ctx.lineTo(this.points[0].x, this.stageHeight);
+        ctx.fill();
+        ctx.closePath();
     }
 }
